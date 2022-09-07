cadb647ceea8bbff704668b85901038cf237b9a9
Javier <javiatitlan@yahoo.es>
2022-09-06 22:18:55 -0600

    Clase Nodo creada

--- Cola.py
+++ Cola.py
@@ -3,5 +3,7 @@ from typing import Generic, TypeVar
 
 T = TypeVar("T")
 class Nodo(Generic[T]):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self,elemento:T) -> None:
+        self.siguiente:Nodo[T] = None
+        self.elemeento:T = elemento
+    
