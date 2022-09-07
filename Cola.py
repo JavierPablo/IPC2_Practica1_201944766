9af3bddc42bcaaf0c9856d21c0ee073d48262f50
Javier <javiatitlan@yahoo.es>
2022-09-06 22:42:32 -0600

    metodo insertar implementado

--- Cola.py
+++ Cola.py
@@ -12,7 +12,12 @@ class Cola(Generic[R]):
     def __init__(self) -> None:
         self.inicio:Nodo[R] = None
         self.final:Nodo[R] = None
-    def insertar(self,element) -> R:
-        pass
+    def insertar(self,elemento:R) -> None:
+        if self.inicio == None:
+            self.inicio = Nodo(elemento)
+            self.final = self.inicio
+            return
+        self.final.siguiente = Nodo(elemento)
+        self.final = self.final.siguiente
     def desencolar(self) -> R:
         pass
