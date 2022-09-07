b7e673935aa471858a29d5db20396a80f434d081
Javier <javiatitlan@yahoo.es>
2022-09-06 22:34:21 -0600

    Clase colao creada

--- Cola.py
+++ Cola.py
@@ -6,4 +6,13 @@ class Nodo(Generic[T]):
     def __init__(self,elemento:T) -> None:
         self.siguiente:Nodo[T] = None
         self.elemeento:T = elemento
-    
+
+R = TypeVar("R")
+class Cola(Generic[R]):
+    def __init__(self) -> None:
+        self.inicio:Nodo[R] = None
+        self.final:Nodo[R] = None
+    def insertar(self,element) -> R:
+        pass
+    def desencolar(self) -> R:
+        pass
